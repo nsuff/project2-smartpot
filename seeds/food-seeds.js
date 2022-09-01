116b26f500dc2175f09d17705d4499538a47b5a4
Nino Suffoletta <nbsuffoletta@gmail.com>
2022-09-01 18:32:32 -0400

    updated seeds

--- seeds/food-seeds.js
+++ seeds/food-seeds.js
@@ -6,7 +6,6 @@ const fooddata = [
     created_at: 'August 18, 2022 09:00:00',
     updated_at: 'August 19, 2022 11:00:00',
     user_id: 2,
-    type_id: 2,
     name: 'Mac and Cheese',
     description: 'macaroni noodles with cheese'
   },
@@ -15,7 +14,6 @@ const fooddata = [
     created_at: 'August 3, 2022 12:30:00',
     updated_at: 'August 3, 2022 12:30:00',
     user_id: 1,
-    type_id: 3,
     name: 'Apple Pie',
     description: 'pie'
   },
