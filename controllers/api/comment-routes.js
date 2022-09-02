902deb95c5fee4f901eb89666093d09b4561a338
Nino Suffoletta <nbsuffoletta@gmail.com>
2022-09-02 13:27:45 -0400

    update comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -16,7 +16,8 @@ router.post('/', (req, res) => {
     Comment.create({
       comment_text: req.body.comment_text,
       user_id: req.session.user_id,
-      post_id: req.body.post_id
+      //user_id: req.body.user_id,
+      potluck_id: req.body.potluck_id
     })
       .then(dbCommentData => res.json(dbCommentData))
       .catch(err => {
--- public/js/ninocommenttest.js
+++ public/js/ninocommenttest.js
@@ -3,24 +3,26 @@
 const testCommentFunction = async (event) => {
     event.preventDefault();
 
-    // const name = document.querySelector('#new-comment-text').value.trim();
-    // const description = document.querySelector('#new-food-description').value.trim();
+    const comment_text = document.querySelector('#new-comment-text').value.trim();
+    const potluck_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+    const user_id = 1;
+    if ( comment_text ) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ comment_text, potluck_id, user_id }),
+            headers: { 'Content-Type': 'application/json'},
+        });
 
-    // if (name && description) {
-    //     const response = await fetch('/api/foods', {
-    //         method: 'POST',
-    //         body: JSON.stringify({ name, description }),
-    //         headers: { 'Content-Type': 'application/json'},
-    //     });
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert('Failed to add food!');
+        }
+    }
 
-    //     if (response.ok) {
-    //         document.location.reload();
-    //     } else {
-    //         alert('Failed to add food!');
-    //     }
-    // }
-
-    alert('this is working');
+    //alert('this is working');
 };
 
 
