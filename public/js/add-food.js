9a651b375dd3d5bbbb275722bec6b1ee3cfe6e7a
Nino Suffoletta <nbsuffoletta@gmail.com>
2022-09-03 14:16:48 -0400

    new working branch

--- public/js/add-food.js
+++ public/js/add-food.js
@@ -1,37 +1,34 @@
 
 
-const newFoodFormHandler = async (event) => {
-  console.log('adsfjk;l');
-  event.preventDefault();
+const testFunction = async (event) => {
+    event.preventDefault();
 
-  const name = document.querySelector('#food-name').value.trim();
-  const description = document.querySelector('#food-description').value.trim();
-  console.log(name);
-  console.log(description);
-  
+    const name = document.querySelector('#new-food-name').value.trim();
+    const description = document.querySelector('#new-food-description').value.trim();
+    const potluck_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+    const user_id = 1;
 
-  if (name && description) {
-    const response = await fetch('/api/foods', {
-      method: 'POST',
-      body: JSON.stringify({ name, description }),
-      headers: { 'Content-Type': 'application/json' },
-    });
 
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to sign up.');
+    if (name && description) {
+        const response = await fetch('/api/foods', {
+            method: 'POST',
+            body: JSON.stringify({ name, description, potluck_id, user_id }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+        //console.log(response);
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert('Failed to add food!');
+        }
     }
-  }
 
-  
-
+    //alert(name + '  ' + description);
 };
 
 
 
-document.querySelector('.food-form').addEventListener('submit', newFoodFormHandler);
-
-document.getElementById('foodform').addEventListener('submit', newFoodFormHandler);
-//const form = document.getElementById('')
-
+document.querySelector('.new-food-form').addEventListener('submit', testFunction);
