088371452388571b4dbcf410a56ff1a8d2509c7e
Nino Suffoletta <nbsuffoletta@gmail.com>
2022-09-05 15:21:47 -0400

    edit working

--- public/js/edit-food.js
+++ public/js/edit-food.js
@@ -1,26 +1,32 @@
 async function deleteFormHandler(event) {
-    // event.preventDefault();
-  
-    // const id = window.location.toString().split('/')[
-    //   window.location.toString().split('/').length - 1
-    // ];
-    // if (1 === 1) {
+    event.preventDefault();
+
+    const name = document.querySelector('#edit-food-name').value.trim();
+    const description = document.querySelector('#edit-food-description').value.trim();
+
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+
+    if (1 === 1) {
       
     
-    //   const response = await fetch(`/api/foods/${id}`, {
-    //     method: 'DELETE'
-    //   });
+      const response = await fetch(`/api/foods/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ name, description }),
+        headers: { 'Content-Type': 'application/json'},
+      });
 
       
-    // }
+    }
   
-    // if (response.ok) {
-    //   document.location.replace('/dashboard/');
-    // } else {
-    //   alert(response.statusText);
-    // }
-    alert('this is working');
-  }
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+    //alert('this is working');
+}
   
-  document.getElementById('edit-btn').addEventListener('click', deleteFormHandler);
+document.getElementById('edit-btn').addEventListener('click', deleteFormHandler);
   
