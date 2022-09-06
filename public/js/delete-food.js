706378338a7036d043f271a11c36e87446faec27
Nino Suffoletta <nbsuffoletta@gmail.com>
2022-09-06 18:41:26 -0400

    delete food

--- public/js/delete-food.js
+++ public/js/delete-food.js
@@ -1,28 +1,36 @@
 async function deleteFormHandler(event) {
     event.preventDefault();
   
+    const sesh = document.getElementById('sessionuser').innerHTML;
+    const food_user = document.getElementById('fooduser').innerHTML;
+    const food_potluck = document.getElementById('foodpotluck').innerHTML;
+    const pid = String(food_potluck);
+    const link = '/potluck/'+pid;
+
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-    if (1 === 1) {
+    if (sesh === food_user) {
       
     
-      const response = await fetch(`/api/foods/${id}`, {
+      var response = await fetch(`/api/foods/${id}`, {
         method: 'DELETE'
       });
 
-      
+      //alert('this is working')
+    } else if (sesh != food_user) {
+      alert('You cannot edit a food you did not make!')
     }
   
     if (response.ok) {
-      document.location.replace('/editfood/');
+      document.location.replace(link);
       //history.go();
       //window.location=document.referrer;
     } else {
       alert(response.statusText);
     }
     //history.back();
-    // alert('this is working');
+    //alert(sesh + food_user + food_potluck + id);
   }
   
   document.getElementById('delete-btn').addEventListener('click', deleteFormHandler);
